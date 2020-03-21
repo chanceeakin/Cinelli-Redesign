30807e17084ee541613482d628af2c74c64710c5
Andrew <andrew.lovato@gmail.com>
2020-03-21 18:42:08 -0400

    fixed responsiveness and hover for navbar

--- public/scripts/navbar.js
+++ public/scripts/navbar.js
@@ -1,24 +1,41 @@
 
 // NAVBAR HOVER
 
-//      // BIKES HOVER
-$( "#bikesButton").hover(
+// BIKES HOVER
+
+ $( "#bikesButton").hover(
     function() {
-        $("#dropdownMenu").css( {"opacity": "1"} ).hover(
-            function (){
-                $("#dropdownMenu").css( "opacity", "1" )
-            }, function() {
-                $("#dropdownMenu").css( "opacity", "0" ).unbind('mouseenter');
-            }
-        );
+        if($( window ).width() > 990) {
+            $("#dropdownMenu").css( {"opacity": "1"}, {"z-index": "1"} ).hover(
+                function (){
+                    $("#dropdownMenu").css( {"opacity": "1"}, {"z-index": "1"})
+                }, function() {
+                    $("#dropdownMenu").css( "opacity", "0" ).unbind('mouseenter');
+                }
+            );
+        }
+        },   function() {
+            $("#dropdownMenu").css( "opacity", "0" );
+        }
     
-    },   function() {
-        $("#dropdownMenu").css( "opacity", "0" );
-        
-    }
+    
  );
 
+   
 
 
 
-
+// $( "#bikesButton").hover(
+//     function() {
+//         $("#dropdownMenu").css( {"opacity": "1"}, {"z-index": "1"} ).hover(
+//             function (){
+//                 $("#dropdownMenu").css( {"opacity": "1"}, {"z-index": "1"})
+//             }, function() {
+//                 $("#dropdownMenu").css( "opacity", "0" ).unbind('mouseenter');
+//             }
+//         );
+    
+//     },   function() {
+//         $("#dropdownMenu").css( "opacity", "0" );
+//     }
+//  );
